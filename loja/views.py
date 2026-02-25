604b131c7aea265ae988f1bf0f858768b9e88315
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-02-24 23:19:59 -0300

    Update views.py

--- loja/views.py
+++ loja/views.py
@@ -2,12 +2,17 @@ import time
 from django.http import HttpResponse
 
 def contador_terminal(request):
-    # O professor quer ver isso aqui no terminal do VS Code:
-    print("\n--- INICIANDO CONTAGEM SOLICITADA ---") 
+    # O professor quer ver estes prints no seu terminal
+    print("\n" + "="*30)
+    print("INICIANDO CONTAGEM MÓDULO 62")
+    print("="*30)
     
     for i in range(1, 11):
-        print(f"Passo: {i}") # Isso aparece no terminal
-        time.sleep(1)        # Isso dá o efeito de "número por número"
+        print(f">>> Número: {i}")
+        time.sleep(1) # Pausa de 1 segundo entre os números
         
-    print("--- CONTAGEM FINALIZADA ---\n")
-    return HttpResponse("Verifique o seu terminal para ver a contagem!")
+    print("="*30)
+    print("CONTAGEM FINALIZADA")
+    print("="*30 + "\n")
+    
+    return HttpResponse("<h1>Sucesso!</h1><p>A contagem de 1 a 10 foi realizada no terminal do VS Code.</p>")
