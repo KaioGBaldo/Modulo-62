a600e13032d91f12613463a3da8fa412b33ae51a
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-03-28 23:22:34 -0300

    Set up WSGI for Django application

--- setup_django/wsgi.py
+++ setup_django/wsgi.py
@@ -1,1 +1,7 @@
+import os
+from django.core.wsgi import get_wsgi_application
 
+# Substitua 'setup_django.settings' pelo caminho correto se sua pasta tiver outro nome
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup_django.settings')
+
+application = get_wsgi_application()
